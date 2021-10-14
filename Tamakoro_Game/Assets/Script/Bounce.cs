2758d1c60727d15976c8b62089894dc7115968ad
KBC12005185\kbc12005185 <kana5665>
2021-10-14 15:35:21 +0900

    特に何も書いてないスクリプトです

---
+++ Tamakoro_Game/Assets/Script/Bounce.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public float speed = 5f;
+    // 速さの最大値を指定する変数を追加
+    public float minSpeed = 5f;
+    // 速さの最小値を指定する変数を追加
+    public float maxSpeed = 10f;
+
+    Rigidbody myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        myRigidbody.velocity = new Vector3(speed, speed, 0f);
+    }
+
+    // 毎フレーム速度をチェックする
+    void Update()
+    {
+        // 現在の速度を取得
+        Vector3 velocity = myRigidbody.velocity;
+        // 速さを計算
+        float clampedSpeed = Mathf.Clamp(velocity.magnitude, minSpeed, maxSpeed);
+        // 速度を変更
+        myRigidbody.velocity = velocity.normalized * clampedSpeed;
+    }
+}
