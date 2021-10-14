03edca850d907d2cf71112e9baaf11d1ba80654d
池原　大樹 <daiki-ikehara-20f@stu.kbc.ac.jp>
2021-10-14 15:30:16 +0900

    スクリプトを微修正しました。

--- Tamakoro_Game/Assets/scripts/floor.cs
+++ Tamakoro_Game/Assets/scripts/floor.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +6,15 @@ public class floor : MonoBehaviour
 {
     float speed = 90;
     float lowspeed = 60;
-​
+
     float MaxRotate = 30;
     float MinRotate = -30;
-​
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -25,7 +26,7 @@ public class floor : MonoBehaviour
         float step = speed * Time.deltaTime;
         float Angle = transform.eulerAngles.z;
 
-        if (Angle > 180)
+        if (Angle> 180)
         {
             // -180～180となるように補正
             Angle = Angle - 360;
@@ -36,7 +37,7 @@ public class floor : MonoBehaviour
             Debug.Log("右");
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, -30), step);
             Debug.Log(Angle);
-​
+
         }
         else if (x == -1)
         {
@@ -44,7 +45,7 @@ public class floor : MonoBehaviour
         }
         else if (y == 1)
         {
-​
+
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(30, 0, 0), step);
         }
         else if (y == -1)
@@ -53,8 +54,13 @@ public class floor : MonoBehaviour
         }
         else
         {
-​
-​
+
+
         }
+
+
+
     }
+
+
 }
