8400de63511d5dce3c56a3892b74f0cdc771f4af
KBC12005185\kbc12005185 <kana5665>
2021-10-20 12:39:32 +0900

    ボールの跳ね方の更新

--- Tamakoro_Game/Assets/Script/Bounce.cs
+++ Tamakoro_Game/Assets/Script/Bounce.cs
@@ -4,28 +4,22 @@ using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
-    public float speed = 5f;
-    // 速さの最大値を指定する変数を追加
-    public float minSpeed = 5f;
-    // 速さの最小値を指定する変数を追加
-    public float maxSpeed = 10f;
-
-    Rigidbody myRigidbody;
-
-    void Start()
+    private void Start()
     {
-        myRigidbody = GetComponent<Rigidbody>();
-        myRigidbody.velocity = new Vector3(speed, speed, 0f);
+        
     }
 
-    // 毎フレーム速度をチェックする
-    void Update()
+    private void Update()
     {
-        // 現在の速度を取得
-        Vector3 velocity = myRigidbody.velocity;
-        // 速さを計算
-        float clampedSpeed = Mathf.Clamp(velocity.magnitude, minSpeed, maxSpeed);
-        // 速度を変更
-        myRigidbody.velocity = velocity.normalized * clampedSpeed;
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            Debug.Log("当たった!");
+        }
     }
 }
+
