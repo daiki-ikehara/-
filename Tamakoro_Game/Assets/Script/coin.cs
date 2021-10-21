24ad6d694ca4cc3da7d465112cb80ca255ebfc57
KBC12005049\kbc12005049 <nanako-tanahara-20f@stu.lbc.ac.jp>
2021-10-21 14:17:46 +0900

    アイテム収集とカウントのスクリプトの追加

---
+++ Tamakoro_Game/Assets/Script/coin.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+	public static int count = 0;
+	void OnCollisionEnter(Collision collision)
+	{
+		// Ballと衝突したら
+		if (collision.gameObject.tag == "Ball")
+		{
+			// アイテムが消える
+			Destroy(gameObject);
+			count = count + 1;
+			Debug.Log(count);
+		}
+	}
+}
