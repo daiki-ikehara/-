66fdbaab849bccce3f8cbe2896e97eb3b9324f84
DESKTOP-P6Q5SBR\NEC-PCuser <eri-nakamura-20f@stu.kbc.ac.jp>
2021-10-27 14:09:10 +0900

    床の変更

--- Tamakoro_Game/Assets/Script/floor.cs
+++ Tamakoro_Game/Assets/Script/floor.cs
@@ -10,12 +10,13 @@ public class floor : MonoBehaviour
     float v = V;
     //	移動量
     float a = 0.3f;//加速度
-
+    
 
     float Minspeed = 60f;
 
     float flag = 0;
     float lowflag = 0;
+    float tmp = 0;
 
     private void Update()
     {
@@ -26,78 +27,100 @@ public class floor : MonoBehaviour
         float y = Input.GetAxis("Vertical");
 
 
-        if (x != 0 || y != 0)
+        if (x != 0||y!=0)//4方向のうちどれかに傾いているとき
         {
-            if (v <= 90f)
-            {
-                v += a;
-            }
+
+
+            tmp = flag;
+
             if (x > 0)
             {
+                
                 flag = 1;
+                
             }
             else if (x < 0)
             {
+                
                 flag = 2;
             }
             else if (y > 0)
             {
                 flag = 3;
+                
             }
             else if (y < 0)
             {
                 flag = 4;
+                
             }
-        }
-        else
-        {
-            if (flag == 1 || flag == 2 || flag == 3 || flag == 4)//離した方向
-            {
 
+            if (v <= 90f)
+            {
+                
+                v += a;
+            }
+
+       
+            
+            
+
+        }
+        else//離されているときの処理
+        {
+           
                 v -= a;//減速
 
-            }
-
+            
+            
+           
             if (v < V)//VこえたらVにもどす
             {
                 v = V;
                 lowflag = 1;
 
+                
+                 
             }
 
-            if (lowflag == 1)
-            {
+          
 
 
-            }
         }
 
-        if (v != V)
+       
+
+        if (v != V)//回転中
         {
             if (flag == 1)
             {
-
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0f, -30), v * Time.deltaTime);
+                
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0f, -30), v*Time.deltaTime);
             }
             if (flag == 2)
             {
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0f, 30), v * Time.deltaTime);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0f, 30), v* Time.deltaTime);
             }
 
-            if (flag == 3)
+            if(flag == 3)
             {
 
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(30, 0f, 0), v * Time.deltaTime);
             }
             if (flag == 4)
             {
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(-30, 0f, 0), v * Time.deltaTime);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(-30, 0f, 0), v* Time.deltaTime);
+            }
+
+            if (tmp!=0&&tmp != flag)
+            {
+                v = V;
+                Debug.Log("Ok");
             }
 
 
         }
 
-        //Debug.Log(v);
 
 
 
