17cb329b228e9fbaf37fc2cd68871dbcbbc3ea6c
shingakitakumi <takumi-shingaki-20f@stu.kbc.ac.jp>
2021-10-28 16:53:57 +0900

    GameObjectのNoneのとこにPausepanel入れてね

---
+++ Tamakoro_Game/Assets/Script/GameEndButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEndButton : MonoBehaviour
+{
+    public void GameEnd()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+}
---
+++ Tamakoro_Game/Assets/Script/ReStartButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReStartButton : MonoBehaviour
+{
+    public void ReStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;  // 再開
+    }
+}
