85e39c89112869cc7a7c1eafeab526a7af012ae9
KBC12005185\kbc12005185 <kana5665>
2021-10-28 09:33:29 +0900

    ボールのPrefabの変更

--- Tamakoro_Game/Assets/Script/Gravity.cs
+++ Tamakoro_Game/Assets/Script/Gravity.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
+    int Time = 0;
     private void Start()
     {
+        
     }
-
     private void Update()
     {
+        this.transform.position += new Vector3(0, -0.005f, 0);
     }
-
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("当たった");
+        while(Time <= 500){
+            if (Time / 5 == 0)
+            {
+                this.transform.position += new Vector3(0, 0.005f, 0);
+            }
+            Time++;
+        }
+        Time = 0;
+    }
 }
