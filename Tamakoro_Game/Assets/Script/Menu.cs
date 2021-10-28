70487f27deb673af5262ee6cf64c3e108ab6cc17
shingakitakumi <takumi-shingaki-20f@stu.kbc.ac.jp>
2021-10-28 16:02:01 +0900

    no message

---
+++ Tamakoro_Game/Assets/Script/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    int xB = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("joystick button 7") && xB == 0)
+        {
+
+        }
+    }
+}
