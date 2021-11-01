5ef80240ca74db563471a9d3c9c7a16107829f01
KBC12005049\kbc12005049 <nanako-tanahara-20f@stu.lbc.ac.jp>
2021-11-01 13:21:17 +0900

    no message

---
+++ Tamakoro_Game/Assets/Script/TitleButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour
+{
+    public void Title()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
