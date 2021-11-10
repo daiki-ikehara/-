feb13e35414390fad76a9b1246661424fecc6369
KBC12005185\kbc12005185 <kana5665>
2021-11-10 11:55:19 +0900

    ボールに関しての修正

--- Tamakoro_Game/Assets/Script/Bounce.cs
+++ Tamakoro_Game/Assets/Script/Bounce.cs
@@ -4,40 +4,13 @@ using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
-    private Transform Object;
-    private Vector3 Center;
-    private float x;
-    private float y;
-    private float z;
-    private float radius = 0.1f;
-    private float cubeX = 1f;
-    private float cubeY = 0.05f;
-    private float cubeZ = 1f;
-
-    // Use this for initialization
     private void Start()
     {
-        Object = GetComponent<Transform>();
-        Center = Object.position;
+        
     }
     private void Update()
     {
-        x = Center.x;
-        y = Center.y;
-        z = Center.z;
-        if (this.gameObject.tag == "Floor")
-        {
-            x = Center.x;
-            y = Center.y;
-            z = Center.z;
-        }
-        if (this.gameObject.tag == "Ball")
-        {
-            
-        }
-        if (this.gameObject.tag == "Coin")
-        {
-        }
+        
     }
 }
 
