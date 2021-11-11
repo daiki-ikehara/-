244298b4dd5e3d1cd344531e01e616d072f4d976
KBC12005185\kbc12005185 <kana5665>
2021-11-11 15:41:32 +0900

    ボールの物理挙動の修正

--- Tamakoro_Game/Assets/Script/Gravity.cs
+++ Tamakoro_Game/Assets/Script/Gravity.cs
@@ -8,20 +8,52 @@ public class Gravity : MonoBehaviour
     private Vector3 pos;
     private float gravity;
     private float totalFallTime = 0f;
+    private float totalBounceTime = 0f;
+    private float bounce = 0.02f;
+    private float height;
+    private float Count = 0;
+    GameObject floor;
+    GameObject ball;
+    GameObject coin;
+    collisiondetection col;
 
     void Start()
     {
         myTransform = GetComponent<Transform>();
         pos = myTransform.position;
         gravity = -9.81f;
+        height = pos.y;
     }
 
     void Update()
     {
-        totalFallTime += Time.deltaTime;
-        pos.y += (gravity * Time.deltaTime) * totalFallTime;         //y座標への加算
+        if (this.gameObject.tag == "Ball")
+        {
 
-        myTransform.position = pos;     //座標を設定
+            floor = GameObject.Find("Cube");
+            col = floor.GetComponent<collisiondetection>();
+            //重力
+            if (col.floorY + 0.1f <= pos.y - col.radius)
+            {
+                totalFallTime += Time.deltaTime;
+                pos.y += (gravity * Time.deltaTime) * totalFallTime;         //y座標への加算
+                myTransform.position = pos;     //座標を設定
+            }
+            else
+            {
+                totalFallTime = 0;
+                while (pos.y <= height / 2 || bounce > 0)
+                {
+                    pos.y += bounce;
+                    bounce -= 0.002f;
+                }
+                height = pos.y;
+            }
+
+        }
+        if (this.gameObject.tag == "Coin")
+        {
+        }
     }
 
 }
