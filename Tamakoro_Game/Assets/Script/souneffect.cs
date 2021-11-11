4348dd51ecb81e25f9486ad26e367d8aed63331a
KBC12005049\kbc12005049 <nanako-tanahara-20f@stu.lbc.ac.jp>
2021-11-11 15:17:03 +0900

    カウントダウンの効果音追加

---
+++ Tamakoro_Game/Assets/Script/souneffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class souneffect : MonoBehaviour
+{
+
+    public AudioClip countsound;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        //Componentを取得
+        audioSource = GetComponent<AudioSource>();
+
+        //音(sound1)を鳴らす
+        audioSource.PlayOneShot(countsound);
+    }
+
+    void Update()
+    {
+
+
+    }
+}
