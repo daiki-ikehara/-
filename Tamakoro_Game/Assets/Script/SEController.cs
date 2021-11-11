ec61da13a83a4713ada443905c3fcc49c97ae744
Ushiku-Hiroto <hiroto_usiku20f@stu.kbc.ac.jp>
2021-11-11 15:27:43 +0900

    BGM、カウントダウンSEの作成

---
+++ Tamakoro_Game/Assets/Script/SEController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEController : MonoBehaviour
+{
+    public AudioClip CountDown;
+    AudioSource SE;
+    void Start()
+    {
+        SE = GetComponent<AudioSource>();
+        SE.PlayOneShot(CountDown);
+    }
+
+    void Update()
+    {
+        
+    }
+}
