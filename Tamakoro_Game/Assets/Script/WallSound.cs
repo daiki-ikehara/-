a2f10ec942ae241ffbd82b986e8abf013a135285
KBC12005049\kbc12005049 <nanako-tanahara-20f@stu.lbc.ac.jp>
2021-11-16 10:21:10 +0900

    壁にぶつかったときの効果音追加と音量調整

---
+++ Tamakoro_Game/Assets/Script/WallSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSound : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ball")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+
+    }
+}
--- Tamakoro_Game/Assets/Script/souneffect.cs
+++ Tamakoro_Game/Assets/Script/souneffect.cs
@@ -4,22 +4,8 @@ using UnityEngine;
 
 public class souneffect : MonoBehaviour
 {
-
-    public AudioClip countsound;
-    AudioSource audioSource;
-
     void Start()
     {
-        //Componentを取得
-        audioSource = GetComponent<AudioSource>();
-
-        //音(sound1)を鳴らす
-        audioSource.PlayOneShot(countsound);
-    }
-
-    void Update()
-    {
-
-
+        GetComponent<AudioSource>().Play();
     }
 }
