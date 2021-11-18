b6040b26658c1f7424bee12b74f0cb0719862f8e
shingakitakumi <takumi-shingaki-20f@stu.kbc.ac.jp>
2021-11-18 16:03:53 +0900

    一つ忘れてたやつ

---
+++ Tamakoro_Game/Assets/Script/StageSelectSceneButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelectSceneButton : MonoBehaviour
+
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown("joystick button 0"))
+        {
+            SceneManager.LoadScene("StageSelect");
+            //Time.timeScale = 1;
+        }
+    }
+}
+
