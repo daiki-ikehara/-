4522936e5c38dbb595a42286e0a954dc78ec42e2
KBC12005049\kbc12005049 <nanako-tanahara-20f@stu.lbc.ac.jp>
2021-11-18 10:56:24 +0900

    収集するとボールが速くなるスクリプト追加

---
+++ Tamakoro_Game/Assets/Script/ball_speed.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_speed : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        Rigidbody rb = this.transform.GetComponent<Rigidbody>();
+        Vector3 force = new Vector3(0.0f, 0.0f, 0.0f);
+
+
+        rb.AddForce(force); // 力を加える
+    }
+}
--- Tamakoro_Game/Assets/Script/item.cs
+++ Tamakoro_Game/Assets/Script/item.cs
@@ -4,24 +4,58 @@ using UnityEngine;
 
 public class item : MonoBehaviour
 {
+    private Vector3 _prevPosition;
+    float speed = 0;
+    public GameObject Heart;
     // Start is called before the first frame update
     void Start()
     {
-
+        _prevPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (speed == 0)
+        {
+            Debug.Log("ぜろ");
+        }
+        else
+        {
+            Debug.Log("加速中");
+        }
     }
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Ball")
+        if (collision.gameObject.tag == "Item")
         {
             // アイテムが消える
-            Destroy(gameObject);
-            //Debug.Log(count);
+            Destroy(Heart);
+            if (Mathf.Approximately(Time.deltaTime, 0))
+                return;
+            speed = 10;
+            // 現在位置取得
+            var position = transform.position;
+
+            // 現在速度計算
+            var velocity = (position - _prevPosition) * speed/ Time.deltaTime;
+
+            // 現在速度をログ出力
+            print($"velocity = {velocity}");
+
+            // 前フレーム位置を更新
+            _prevPosition = position;
         }
+        StartCoroutine("speedstop");
+
+    }
+
+    IEnumerator speedstop()
+    {
+        for (int i = 0; i < 450; i++)
+        {
+            yield return null;
+        }
+        speed = 0;
     }
 }
