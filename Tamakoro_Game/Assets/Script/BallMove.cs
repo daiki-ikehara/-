099532475bf82be5fda7b78440669305dc1e687d
KBC12005185\kbc12005185 <kana5665>
2021-11-24 15:08:33 +0900

    ボールのスクリプトの変更

--- Tamakoro_Game/Assets/Script/BallMove.cs
+++ Tamakoro_Game/Assets/Script/BallMove.cs
@@ -20,30 +20,51 @@ public class BallMove : MonoBehaviour
         ball = GameObject.Find("Sphere");
         gravity = ball.GetComponent<Gravity>();
 
-        if (col.RotateX > 0)
+        if (col.RotateX > 0 && col.RotateZ == 0)
         {
             col.floorY = col.RotateX / -55f;
             col.floorY = col.floorY * (gravity.pos.z);
         }
-        if (col.RotateX < 0)
+        if (col.RotateX < 0 && col.RotateZ == 0)
         {
             col.floorY = col.RotateX / -55f;
             col.floorY = col.floorY * (gravity.pos.z);
         }
-        if(col.RotateZ > 0)
+        if (col.RotateZ > 0 && col.RotateX == 0)
         {
             col.floorY = col.RotateZ / 55f;
             col.floorY = col.floorY * (gravity.pos.x);
         }
-        if (col.RotateZ < 0)
+        if (col.RotateZ < 0 && col.RotateX == 0)
         {
             col.floorY = col.RotateZ / 55f;
             col.floorY = col.floorY * (gravity.pos.x);
         }
 
-        if (col.floorX + 0.95f >= gravity.pos.x + col.radius && col.floorX - 0.95f <= gravity.pos.x - col.radius)
+        if (col.RotateX > 0 && col.RotateZ > 0)
         {
-            if (col.floorZ + 0.95f >= gravity.pos.z + col.radius && col.floorZ - 0.95f <= gravity.pos.z - col.radius)
+            col.floorY = col.RotateX / -30f;
+            col.floorY = col.floorY * (gravity.pos.z);
+        }
+        if (col.RotateX < 0 && col.RotateZ < 0)
+        {
+            col.floorY = col.RotateX / -30f;
+            col.floorY = col.floorY * (gravity.pos.z);
+        }
+        if (col.RotateZ > 0 && col.RotateX < 0)
+        {
+            col.floorY = col.RotateZ / 30f;
+            col.floorY = col.floorY * (gravity.pos.x);
+        }
+        if (col.RotateZ < 0 && col.RotateX > 0)
+        {
+            col.floorY = col.RotateZ / 30f;
+            col.floorY = col.floorY * (gravity.pos.x);
+        }
+
+        if (col.floorX + 0.95f + col.RotateZ / 600f >= gravity.pos.x + col.radius && col.floorX - 0.95f + col.RotateZ / 600f <= gravity.pos.x - col.radius)
+        {
+            if (col.floorZ + 0.95f - col.RotateX / 600f >= gravity.pos.z + col.radius && col.floorZ - 0.95f - col.RotateX / 600f <= gravity.pos.z - col.radius)
             {
                 if (col.RotateX > 0)
                 {
@@ -62,22 +83,22 @@ public class BallMove : MonoBehaviour
                     gravity.pos.x += BallSpeed;
                 }
             }
-            else if(col.floorZ + 0.95f < gravity.pos.z + col.radius)
+            else if(col.floorX + 0.95f + col.RotateZ / 600f < gravity.pos.x + col.radius)
             {
-                gravity.pos.z = col.floorZ + 0.95f - col.radius;
+                gravity.pos.z = col.floorZ + 0.95f - col.radius + col.RotateZ / 600f;
             }
-            else if (col.floorZ - 0.95f > gravity.pos.z - col.radius)
+            else if (col.floorX - 0.95f + col.RotateZ / 600f > gravity.pos.x - col.radius)
             {
-                gravity.pos.z = col.floorZ - 0.95f + col.radius;
+                gravity.pos.z = col.floorZ - 0.95f + col.radius + col.RotateZ / 600f;
             }
         }
-        else if (col.floorX + 0.95f < gravity.pos.x + col.radius)
+        else if (col.floorX + 0.95f - col.RotateX / 600f < gravity.pos.x + col.radius)
         {
-            gravity.pos.x = col.floorX + 0.95f - col.radius;
+            gravity.pos.x = col.floorX + 0.95f - col.radius - col.RotateX / 600f;
         }
-        else if (col.floorX - 0.95f > gravity.pos.x - col.radius)
+        else if (col.floorX - 0.95f - col.RotateX / 600f > gravity.pos.x - col.radius)
         {
-            gravity.pos.x = col.floorX - 0.95f + col.radius;
+            gravity.pos.x = col.floorX - 0.95f + col.radius - col.RotateX / 600f;
         }
     }
 }
